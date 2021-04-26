2a7620720d19d7d466079d9c2963944f6c887777
keleearth <1582154644@qq.com>
2021-04-26 17:25:31 +0800

    feat gj/ : 生成统计图界面编写

--- gj/gj.py
+++ gj/gj.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
 #用于处理数据
 class Actuator(object):
 
@@ -82,9 +84,57 @@ class Actuator(object):
         self.dataLocation = fileLocation
         #存放从文件中读取出来的数据
         self.data = []
+        #ID词汇表
+        self.dataSet = set([])
+        #数据集合
+        self.yearData = {}
+        #词集字典，方便确认位置
+        self.dataDict = {}
         #----找到数据中共有多少字段
-        self.dataSet = set(["DA"])
-    #从文件中读取数据并解析
+        #self.dataSet = set(["DA"])
+    #对词集进行编码方便确定位置
+    def __encodedDataSet(self):
+        index = 0
+        for dictKey in self.dataSet:
+            self.dataDict[dictKey] = index
+            self.dataDict[index] = dictKey
+            index = index + 1
+
+    #根据词集和数据集合统计个数
+    def countYearWordNum(self):
+        self.__encodedDataSet()
+        for key in self.yearData:
+            #每一年的数据集
+            #print(key)
+            self.yearData[key] = self.__count(self.yearData[key])
+    
+    def __count(self,yearData):
+        
+        dataCount = [0]*len(self.dataSet)
+        for data in yearData:
+            if(data in self.dataSet):
+                dataCount[self.dataDict[data]
+                          ] = dataCount[self.dataDict[data]] + 1
+        return dataCount
+
+
+    #对ID列进行分词并生成词表和每一年的词集
+    def produceVocabSet(self):
+        for vocab in self.data:
+            if 'ID' in vocab and 'PY' in vocab:
+                #print(vocab['ID'])
+                yearDataList = vocab["ID"].strip().replace("_\t_", " ").split(";")
+                #print(yearDataList)
+                yearKey = vocab.get('PY',"1970")
+                if yearKey in self.yearData:
+                    #每一年的数据集
+                    self.yearData[yearKey].extend(yearDataList)
+                else:
+                    self.yearData[yearKey] = yearDataList
+            #     print(vocab["ID"].replace("_\t_", ";").split(";"))
+                #词表集合
+                self.dataSet = self.dataSet | set(yearDataList)
+    #读取文件中数据 
     def readData(self):
         with open(self.dataLocation, 'r',encoding='utf-8') as dataFile:
             # for line in dataFile.readlines():
@@ -92,9 +142,9 @@ class Actuator(object):
             dataFile.readline()
             dataFile.readline()
             while True:
-                data = self.__readOneData(dataFile)
-                if(data):
-                    self.data.append(data)
+                oneData = self.__readOneData(dataFile)
+                if(oneData):
+                    self.data.append(oneData)
                 else:
                    break
     #只读取数据文件中的一条数据
@@ -104,18 +154,21 @@ class Actuator(object):
         line = dataFile.readline()
         #数据结构每一字段包含的数据
         column=""
-        cloumnData=[]
+        cloumnData=""
+        #cloumnData=[]
         while line:
             if(line.startswith('\n')):
                 break
             else:
                 if(line.startswith(' ')):
-                    dataStruct[column].append(line[3:].strip('\n'))
+                    dataStruct[column] = dataStruct[column] + "_\t_" + line[3:].strip('\n')
+                    #dataStruct[column].append(line[3:].strip('\n'))
                 else:
                     column = line[0:2]
-                    cloumnData = [line[3:].strip('\n')]
+                    cloumnData = line[3:].strip('\n')
+                    #cloumnData = [line[3:].strip('\n')]
                     dataStruct[column] = cloumnData
-                    self.dataSet.add(column)
+                    #self.dataSet.add(column)
                 # self.data.append(line)
                 line = dataFile.readline()
         return dataStruct
@@ -124,25 +177,111 @@ class Actuator(object):
     def printData(self):
         for data in self.data:
             print(data)
+    #找到数量最多的单词
+    def searchTopWords(self,num):
+        #将各年度数据相加得到数量
+        totalNum = np.zeros(len(self.dataSet), int)
+        for yearData in self.yearData:
+            totalNum = totalNum + np.array(self.yearData[yearData])
+        wordList = []
+        #数组长度
+        length = len(totalNum)
+        #拿到数量前100的词汇集合
+        if length < num:
+            wordList =[]
+        else:    
+            wordListIndex = np.argpartition(totalNum, 0 - num)
+            for i in range(0,num):
+                wordList.append(self.dataDict[wordListIndex[length-1-i]])
+
+        #print(totalNum)
+        return wordList
+
+    #处理汇总后的数据数据
+    def dealData(self, wordList):
+        resultData = []
+        # columunDataName = ['year']
+        # columunDataName.extend(wordList)
+        # resultData.append(columunDataName)
+        for oneYearData in self.yearData:
+            dataList = self.__dealOneYearData(self.yearData[oneYearData],wordList)
+            dt = []
+            dt.append(oneYearData)
+            dt = dt+dataList 
+            resultData.append(dt)
+        self.resultData = resultData
+       # return resultData
+
+    def __dealOneYearData(self,oneYearData,wordList):
+        dataList = [] 
+        for word in wordList:
+            #print(self.dataDict[word])
+            index = self.dataDict[word]
+            #print(index)
+            dataList.append(oneYearData[index])
+        return dataList
 
     #使用pandas对数据进行可视化
-    def showData(self):
-        dataFrame = pd.DataFrame(self.data)
-                
+    def showData(self,wordList):
+        columunDataName = ['year']
+        columunDataName.extend(wordList)
+        dataFrame = pd.DataFrame(self.resultData, columns=columunDataName)
+        mpl.rcParams['font.sans-serif'] = ['KaiTi', 'SimHei',
+                                           'FangSong']  # 汉字字体,优先使用楷体，如果找不到楷体，则使用黑体
+        mpl.rcParams['font.size'] = 12  # 字体大小
+        mpl.rcParams['axes.unicode_minus'] = False
+        dataFrame = dataFrame.sort_values(by='year', ascending=True)
+
+        dataFrame = dataFrame.set_index(['year'])
+        dataFrame = dataFrame.T
+        dataFrame = dataFrame.reset_index()
+        #画图
+        #绘制画布
+        fig, ax = plt.subplots()
+        ax.set_xlabel('年份',loc='right',labelpad= 5)
+        ax.set_yticklabels(dataFrame["index"])
+        ax.set_yticks(range(len(dataFrame["index"])))
+        ax.set_ylabel('单词')
+        ax.set_xticklabels(dataFrame.columns.tolist()[1:-1])
+        ax.set_xticks(range(len(dataFrame.columns.tolist()[1:-1 ])))
+        #ax.grid(True)
+        ax.tick_params(axis='x', rotation=70)
+        ax.pcolor(dataFrame.iloc[0:-1, 1: -1], edgecolors='k', linewidths=1)
+        im = plt.imshow(dataFrame.iloc[0:-1, 1: -1])
+        plt.colorbar(im)
+        return fig
+        #print(dataFrame)
+        #plt.show()
+        #id 和py字段画图
 
 
-if __name__ == '__main__':
-    actuator = Actuator('E:\\project\\python\\data.txt')
-    actuator.readData()
-    # actuator.printData()
-    # print(actuator.dataSet)
-    # print(actuator.data[0])
-    dictData = actuator.data[0]
-    #将data[0]输出到文件
-    with open('data\\test.txt', 'w', encoding='utf-8') as testDoc:
-        for key in dictData:
-            testDoc.write(key + '\t')
-            testDoc.write(str(dictData[key]) + "\n")
-        testDoc.write(str(actuator.dataSet))
+# if __name__ == '__main__':
+#     actuator = Actuator('E:\\project\\python\\data.txt')
+#     actuator.readData()
+#     # actuator.printData()
+#     # print(actuator.dataSet)
+#     # print(actuator.data[0])
+#     # dictData = actuator.data[0]
+#     actuator.produceVocabSet()
+#     actuator.countYearWordNum()
+#     wordList = actuator.searchTopWords(30)
+#     actuator.dealData(wordList)
+#     actuator.showData()
+#     #将data[0]输出到文件
+#     with open('data\\test.txt', 'w', encoding='utf-8') as testDoc:
+#         # for key in dictData:
+#         #     testDoc.write(key + '\t')
+#         #     testDoc.write(str(dictData[key]) + "\n")
+#         #testDoc.write(str(actuator.dataSet))
+#         # for dt in actuator.dataSet:
+#         #     testDoc.write(str(dt))
+#         for key in actuator.yearData:
+#             testDoc.write(str(key) + ":" + str(actuator.yearData[key]) + "\n")
+#         for key in actuator.dataDict:
+#             testDoc.write(str(key) + ":" + str(actuator.dataDict[key]) + "\n")
+#         testDoc.write(str(wordList) + "\n")
+#         for dd in actuator.resultData:
+#             testDoc.write(str(dd)+"\n")
+    #actuator.showData()
 
     # print("aa")
--- gj/ui.py
+++ gj/ui.py
@@ -1,13 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import tkinter
+import tkinter as tk
+from tkinter import filedialog
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
+import gj
 
-top = tkinter.Tk()
-top.title("小工具")
-top.geometry("585x350+200+200")
-top.mainloop()
 
+class UiApp(object):
+    def __init__(self):
+        self.topWindow = tk.Tk()
+        self.actuator =  {}
+        self.filename = ""
+    
+    def __openFileWindow(self):
+        self.filename = filedialog.askopenfilename(title = "选择数据文件")
 
+    #画图
+    def __drawPc(self):
+        if(len(self.filename)==0):
+            tk.messagebox.showinfo('提醒', '选择一份数据文件')
+        else:
+            actuator = gj.Actuator(self.filename)
+            actuator.readData()
+            actuator.produceVocabSet()
+            actuator.countYearWordNum()
+            wordList = actuator.searchTopWords(30)
+            actuator.dealData(wordList)
+            fig = actuator.showData(wordList)
+            
+            canvas = FigureCanvasTkAgg(fig, master=self.topWindow)
+            canvas.draw()
+
+            toolbar = NavigationToolbar2Tk(canvas, self.topWindow, pack_toolbar=False)
+            toolbar.update()
+
+            canvas.mpl_connect(
+                "key_press_event", lambda event: print(f"you pressed {event.key}"))
+            canvas.mpl_connect("key_press_event", key_press_handler)
+
+            # button = tk.Button(master=self.topWindow, text="Quit", command=self.topWindow.quit)
+            # button.pack(side=tk.BOTTOM)
+            toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+            canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def run(self):
+        
+        self.topWindow.title("小工具")
+        self.topWindow.geometry("800x520+200+200")
+        #创建菜单
+        menubar = tk.Menu(self.topWindow)
+
+        #选择文件的按钮
+        menubar.add_command(label="文件", command=self.__openFileWindow)
+        #作出统计图的按钮
+        menubar.add_command(label="统计图", command=self.__drawPc)
+        #添加退出按钮
+        menubar.add_command(label="退出程序", command=self.topWindow.quit)
+
+        self.topWindow.config(menu=menubar)
+        self.topWindow.mainloop()
+ 
 if __name__ == '__main__':
-    print("cc")
+    app = UiApp()
+    app.run()
+    
+
